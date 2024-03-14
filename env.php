39062c0f5b78be69c843776d8e778f94ed9592a0
VictorVFM <you@example.com>
2024-03-14 17:05:01 -0300

    Adicionando docker-compose

--- env.php
+++ env.php
@@ -1,8 +1,7 @@
 <?php
-$DB_HOSTNAME = 'monorail.proxy.rlwy.net';
-$DB_PORT = '40384';
+$DB_HOSTNAME = '172.17.0.1';
+$DB_PORT = '3309';
 $DB_USERNAME = 'root';
-$DB_PASSWORD = '165A5c1GeDgFCCgAEc26Ag4h3ghEEdFG';
-$DB_DATABASE = 'querycar';
-
+$DB_PASSWORD = 'Qu3ryC47';
+$DB_DATABASE = 'query_car';
 ?>
